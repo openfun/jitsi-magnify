6ade171829cbac5cb438e3e0d351c42d72cb33e8
AlexLaval <alex.laval.g@gmail.com>
2022-06-16 13:41:06 +0200

    ✨(frontend) add sidebar button component
    The sidebar needs special buttons
    that have different styles depending on
    the current page.
    In order to render routed components such as buttons,
    add a mock router to the storybook.

--- src/frontend/magnify/src/components/index.ts
+++ src/frontend/magnify/src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as TestButton, TestButtonVariant } from './TestButton';
-
 export * from './design-system';
 export * from './groups';
 export * from './profile';
-export * from './design-system';
+export * from './sidebar';
