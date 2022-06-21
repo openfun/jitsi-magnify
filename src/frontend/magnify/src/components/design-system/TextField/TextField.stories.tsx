3efe66673b9f4d3e4371bfd3d77579d8e71e55d4
Léo-Nils Boissier <leo.nils2000@gmail.com>
2022-06-21 10:37:26 +0200

    ✨(frontlib) create a custom text field component
    Gromet formfield inputs are uggly... Even by creating a
    custom style I couldn't get a correct one. This is an attempt
    to have something closer to the suggestion of the designer

---
+++ src/frontend/magnify/src/components/design-system/TextField/TextField.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import TextField from './TextField';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'DesignSystem/TextField',
+  component: TextField,
+} as ComponentMeta<typeof TextField>;
+
+const Template: ComponentStory<typeof TextField> = (args) => <TextField {...args} />;
+
+const baseArgs = {
+  label: 'My input',
+  name: 'my-input',
+  onChange: console.log,
+  value: 'xxx',
+};
+
+// create the template and 2 stories for variants blue and red
+export const Simple = Template.bind({});
+Simple.args = { ...baseArgs };
+
+export const WithMargin = Template.bind({});
+WithMargin.args = {
+  ...baseArgs,
+  margin: 'large',
+};
+
+export const WithErrors = Template.bind({});
+WithErrors.args = {
+  ...baseArgs,
+  errors: ['This is an error'],
+};
