12efe85cb78a72da806a8e585c8179254e178b57
AlexLaval <alex.laval.g@gmail.com>
2022-06-27 18:50:41 +0200

    ✅(frontend) add tests for sidebar components
    The major part of tests are
    in the button component.
    The Sidebar is not responsible for
    the button to be correctly displayed when clicked

---
+++ src/frontend/magnify/src/components/sidebar/SidebarButton/SidebarButton.test.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+import SidebarButton from './SidebarButton';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { MemoryRouter, Router } from 'react-router-dom';
+
+describe('SidebarButton', () => {
+  it('should render with correct label', () => {
+    render(
+      <IntlProvider locale="en">
+        <MemoryRouter>
+          <SidebarButton label="Test Button" navigateTo="test" />
+        </MemoryRouter>
+      </IntlProvider>,
+    );
+
+    const button = screen.getByRole('link');
+    expect(button).toHaveTextContent('Test Button');
+  });
+
+  it('should redirect to a path when clicked', () => {
+    const history = createMemoryHistory();
+    history.push('/');
+
+    render(
+      <IntlProvider locale="en">
+        <Router location={history.location} navigator={history}>
+          <SidebarButton label="Test Button" navigateTo="test" />
+        </Router>
+      </IntlProvider>,
+    );
+
+    const button = screen.getByRole('link', { name: 'Test Button' });
+    expect(button).toHaveAttribute('href', '/test');
+    fireEvent.click(button);
+    expect(history.location.pathname).toBe('/test');
+  });
+
+  it('should be transparent when location does not match its path', () => {
+    const history = createMemoryHistory();
+    history.push('/any/path');
+
+    render(
+      <IntlProvider locale="en">
+        <Router location={history.location} navigator={history}>
+          <SidebarButton label="Test Button" navigateTo="test" />
+        </Router>
+      </IntlProvider>,
+    );
+
+    const button = screen.getByRole('link', { name: 'Test Button' });
+    expect(button).toHaveStyle('background-color: transparent');
+  });
+
+  it('should be colored when location root matches its given path', async () => {
+    const history = createMemoryHistory();
+    history.push('/test/any');
+
+    render(
+      <IntlProvider locale="en">
+        <Router location={history.location} navigator={history}>
+          <SidebarButton label="Test Button" navigateTo="test" />
+        </Router>
+      </IntlProvider>,
+    );
+
+    const button = screen.getByRole('link', { name: 'Test Button' });
+    expect(button).not.toHaveStyle('background-color: transparent');
+  });
+});
