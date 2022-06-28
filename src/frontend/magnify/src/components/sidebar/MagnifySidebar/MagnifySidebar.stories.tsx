5c565781b2db362c3aaa550b61e2fde6c90e0075
AlexLaval <alex.laval.g@gmail.com>
2022-06-28 09:24:22 +0200

    ✨(frontend) add sidebar main components
    Add a LayoutWithSidebar component
    that renders its children
    at the sidebar's right.
    This sidebar accepts as props
    lists of lists of button props.
    Dev doesn't need to give the style
    for each button,
    it's the sidebar's role.

---
+++ src/frontend/magnify/src/components/sidebar/MagnifySidebar/MagnifySidebar.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import LayoutWithSidebar from './MagnifySidebar';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { AppsRounded } from 'grommet-icons';
+
+export default {
+  title: 'Sidebar/MagnifySidebar',
+  component: LayoutWithSidebar,
+} as ComponentMeta<typeof LayoutWithSidebar>;
+
+const Template: ComponentStory<typeof LayoutWithSidebar> = (args) => (
+  <LayoutWithSidebar {...args}></LayoutWithSidebar>
+);
+
+// create the template and stories
+export const customSidebar = Template.bind({});
+customSidebar.args = {
+  itemZones: [
+    [
+      {
+        label: 'Button A',
+        navigateTo: 'a',
+        icon: <AppsRounded />,
+      },
+      {
+        label: 'Button B',
+        navigateTo: 'b',
+        icon: <AppsRounded />,
+      },
+    ],
+    [
+      {
+        label: 'Button C',
+        navigateTo: 'c',
+        icon: <AppsRounded />,
+      },
+      {
+        label: 'Button D',
+        navigateTo: 'd',
+        icon: <AppsRounded />,
+      },
+    ],
+  ],
+};
