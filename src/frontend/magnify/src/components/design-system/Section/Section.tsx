907f4530d542defb7e21c5608a79269dcf7e67cd
AlexLaval <alex.laval.g@gmail.com>
2022-07-01 12:17:48 +0200

    ✨(frontend) add section component
    Sections are boxes with titles and shadows
    whose content is passed as children.
    This will enable us to build pages.

---
+++ src/frontend/magnify/src/components/design-system/Section/Section.tsx
@@ -0,0 +1,21 @@
+import { Box, Heading, HeadingProps } from 'grommet';
+import React from 'react';
+
+export interface SectionProps {
+  title?: string;
+  titleLevel?: HeadingProps['level'];
+  children?: React.ReactNode;
+}
+
+const Section = ({ children, title, titleLevel = 3 }: SectionProps) => (
+  <Box background={'white'} round="xsmall" elevation="small" pad="medium">
+    {title && (
+      <Heading color="brand" level={titleLevel} margin={{ top: '0' }}>
+        {title}
+      </Heading>
+    )}
+    {children}
+  </Box>
+);
+
+export default Section;
