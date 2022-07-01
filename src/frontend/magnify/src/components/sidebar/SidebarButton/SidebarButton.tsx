5defaa01286de4d5baff62c1a0cbbf9cb1f6d3b5
Léo-Nils Boissier <leo.nils2000@gmail.com>
2022-07-01 21:38:17 +0200

    💄(frontlib) improve styling of the sidebar buttons
    Until now, the hover effects were made with onMouseEnver/leave events
    Here is an attemps to use styled components instead. At first tests
    it is far less buggy and more reactive.

--- src/frontend/magnify/src/components/sidebar/SidebarButton/SidebarButton.tsx
+++ src/frontend/magnify/src/components/sidebar/SidebarButton/SidebarButton.tsx
@@ -1,7 +1,8 @@
-import { Box, Button } from 'grommet';
+import { Box, Button, ThemeType } from 'grommet';
 import { MarginType } from 'grommet/utils';
 import React from 'react';
 import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import styled from 'styled-components';
 
 export interface SidebarButtonProps {
   disabled?: boolean;
@@ -11,38 +12,44 @@ export interface SidebarButtonProps {
   navigateTo: string;
 }
 
+const conditionalColor =
+  (colorActive: string, colorInnactive: string) =>
+  ({ theme, active }: { theme: ThemeType; active: boolean }) =>
+    active
+      ? theme?.global?.colors?.[colorActive] || colorActive
+      : theme?.global?.colors?.[colorInnactive] || colorInnactive;
+
+const HoverableButton = styled(Button)`
+  background-color: ${conditionalColor('brand', 'transparent')};
+  color: ${conditionalColor('light-1', 'dark-1')};
+  & path {
+    fill: ${conditionalColor('light-1', 'dark-1')};
+  }
+  border-color: ${conditionalColor('brand', 'transparent')};
+  &:hover {
+    background-color: ${conditionalColor('brand', 'light-2')};
+    border-color: ${conditionalColor('brand', 'transparent')};
+    box-shadow: ${(props: { theme: ThemeType; active: boolean }) =>
+      props.active ? '0px 0px 0px 1px ' + props.theme?.global?.colors?.brand : 'none'};
+  }
+`;
+
 export default function SidebarButton(props: SidebarButtonProps) {
-  const [hover, setHover] = React.useState(false);
-  const [customColor, setCustomColor] = React.useState('');
   const { navigateTo, label, margin, disabled, icon } = props;
   const resolvedPath = useResolvedPath(navigateTo);
   const match = useMatch({ path: resolvedPath.pathname, end: false });
   const active = match != null;
 
-  React.useEffect(() => {
-    setCustomColor(active ? '' : hover ? 'light-3' : 'transparent');
-  }, [hover, active]);
-
   return (
     <Box margin={margin}>
-      <Button
+      <HoverableButton
         primary
-        color={customColor}
+        active={active}
         disabled={disabled}
         label={label}
         icon={icon}
-        as={({ children, type, className }) => (
-          <Link
-            type={type}
-            className={className}
-            to={navigateTo}
-            onMouseEnter={() => {
-              setHover(true);
-            }}
-            onMouseLeave={() => {
-              setHover(false);
-            }}
-          >
+        forwardedAs={({ children, type, className }) => (
+          <Link type={type} className={className} to={navigateTo}>
             {children}
           </Link>
         )}
