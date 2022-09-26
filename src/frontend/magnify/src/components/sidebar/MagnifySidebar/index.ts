75455ec80bb7c5e27f3bce841f84d63bffdc652d
Nathan Panchout <nathanpanchout@live.fr>
2022-09-26 16:19:23 +0200

    🔥(frontend) delete old layout files
    We no longer use the old Layout

--- src/frontend/magnify/src/components/sidebar/MagnifySidebar/index.ts
+++
@@ -1,1 +0,0 @@
-export { default, MagnifySidebarProps } from './MagnifySidebar';
--- src/frontend/magnify/src/components/sidebar/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as LayoutWithSidebar } from './LayoutWithSidebar';
-export { default as MagnifySidebar, MagnifySidebarProps } from './MagnifySidebar';
-export { default as SidebarButton, SidebarButtonProps } from './SidebarButton';
