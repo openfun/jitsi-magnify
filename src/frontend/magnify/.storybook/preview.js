07338078ed3f7e29b84d94d3f3e5efd27b289315
Nathan Panchout <nathanpanchout@live.fr>
2022-10-11 14:39:23 +0200

    🔧(frontend) update Storybook preview.js
    To use MagnifyProvider inside storybook

--- src/frontend/magnify/.storybook/preview.js
+++ src/frontend/magnify/.storybook/preview.js
@@ -1,12 +1,4 @@
-import { defaultTheme } from '../src';
-import { Grommet } from 'grommet';
-import { MemoryRouter, Routes, Route } from 'react-router-dom';
-import { IntlProvider } from 'react-intl';
-import messages from '../src/translations/fr-FR.json';
-import { ControllerProvider, LogController } from '../src/controller';
-import { QueryClientProvider, QueryClient } from 'react-query';
-import DebugRoute from '../src/utils/DebugRoute';
-import DebugRefreshToken from '../src/utils/DebugRefreshToken';
+import {MagnifyProvider} from '../src';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -18,51 +10,18 @@ export const parameters = {
   },
 };
 
-const controller = new LogController(process.env.TEST_TOKEN);
-controller.refreshActivated = localStorage.getItem('debugRefreshToken') === 'true';
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
 
-// Grommet wrapper decorator
-const withGrommet = (storyFn) => <Grommet full theme={defaultTheme}>{storyFn()}</Grommet>;
 
-// Simulate router behavior
-
-export const withRouter = (storyFn) => (
-  <MemoryRouter>
-    {storyFn()}
-    <Routes>
-      <Route path="*" element={<DebugRoute />} />
-    </Routes>
-  </MemoryRouter>
-);
-
-// Intl wrapper decorator
-const withIntl = (storyFn) => (
-  <IntlProvider locale="fr" messages={messages}>
-    {storyFn()}
-  </IntlProvider>
-);
-
-// Controller to access the backend. Here we use a console.log controller
-const withController = (storyFn) => {
-  return (
-    <>
-      <ControllerProvider controller={controller}>{storyFn()}</ControllerProvider>
-      <DebugRefreshToken controller={controller} />
-    </>
-  );
-};
-
-// query decorator
-const withQuery = (storyFn) => (
-  <QueryClientProvider client={queryClient}>{storyFn()}</QueryClientProvider>
+const withMagnify = (storyFn) => (
+    <MagnifyProvider initialUser={{
+      email: 'john.doe@gmail.com',
+      username: 'JohnDoe',
+      name: 'John Doe'
+    }}>
+      {storyFn()}
+    </MagnifyProvider>
 );
 
 // Decorators
-export const decorators = [ withGrommet, withIntl, withController, withQuery];
+// export const decorators = [ withAuthContext, withGrommet, withIntl, withController, withQuery];
+export const decorators = [withMagnify];
