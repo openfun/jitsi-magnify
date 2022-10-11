159ff301d22d4df962c704f4c2e7f92b33c095d3
Nathan Panchout <nathanpanchout@live.fr>
2022-10-11 14:28:52 +0200

    ✨(frontend) api routes constants
    User and room api routes constants

---
+++ src/frontend/magnify/src/utils/routes/api/rooms/index.ts
@@ -0,0 +1,7 @@
+export enum RoomsApiRoutes {
+  GET_ALL = '/rooms/',
+  CREATE = '/rooms/',
+  UPDATE = '/rooms/:id/',
+  GET = '/rooms/:id/',
+  DELETE = '/rooms/:id/',
+}
---
+++ src/frontend/magnify/src/utils/routes/api/users.routes.ts
@@ -0,0 +1,9 @@
+export enum UsersRoutes {
+  REFRESH_TOKEN = '/accounts/token-refresh/',
+  ME = '/users/me/',
+  LOGIN = '/users/login/',
+  CHANGE_PASSWORD = '/users/change-password/',
+  UPDATE = '/users/:id/',
+  DELETE = '/users/:id/',
+  CREATE = '/users/',
+}
