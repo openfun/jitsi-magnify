59aefb183836e87b0fc0d0a227fee03fbc978a90
Nathan Panchout <nathanpanchout@live.fr>
2022-10-11 15:08:08 +0200

    ✨(frontend) create default Account View
    - Create default Account View
    - Update many account components to connect with api

---
+++ src/frontend/magnify/src/views/profile/account/ProfileAccountView.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ProfileAccountView } from './index';
+
+export default {
+  title: 'Views/Account/ProfileAccountView',
+  component: ProfileAccountView,
+} as ComponentMeta<typeof ProfileAccountView>;
+
+const Template: ComponentStory<typeof ProfileAccountView> = (args) => {
+  return (
+    <RouterProvider
+      router={createBrowserRouter([
+        {
+          path: '*',
+          element: <ProfileAccountView {...args} />,
+        },
+      ])}
+    />
+  );
+};
+
+// create the template and stories
+export const basic = Template.bind({});
