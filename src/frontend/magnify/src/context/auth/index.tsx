52025bd898da3770a57a9338603167d848b0af55
Nathan Panchout <nathanpanchout@live.fr>
2022-10-11 14:31:41 +0200

    ✨(frontend) create AuthContext
    To have the current user everywhere in the application

---
+++ src/frontend/magnify/src/context/auth/index.tsx
@@ -0,0 +1,50 @@
+import { useQueryClient } from '@tanstack/react-query';
+import React, { useEffect, useState } from 'react';
+import { Maybe } from 'yup/es/types';
+import { User } from '../../types/entities/user';
+import { MagnifyQueryKeys } from '../../utils/constants/react-query';
+
+export interface AuthContextInterface {
+  user: Maybe<User> | null;
+  updateUser: (user: Maybe<User> | null) => void;
+}
+
+const AuthContext = React.createContext<Maybe<AuthContextInterface>>(undefined);
+
+type AuthContextProviderProps = {
+  children: React.ReactNode;
+  initialUser?: Maybe<User> | null;
+};
+
+export const AuthContextProvider = ({ children, ...props }: AuthContextProviderProps) => {
+  const [user, setUser] = useState<Maybe<User> | null>(props.initialUser ?? undefined);
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (props.initialUser != null) {
+      queryClient.setQueryData([MagnifyQueryKeys.AUTH_USER], props.initialUser);
+    }
+  }, []);
+
+  const context: AuthContextInterface = React.useMemo(
+    () => ({
+      user: user,
+      updateUser: (user: Maybe<User> | null) => {
+        queryClient.setQueryData([MagnifyQueryKeys.AUTH_USER], user);
+        setUser(user);
+      },
+    }),
+    [user],
+  );
+  return <AuthContext.Provider value={context}>{children}</AuthContext.Provider>;
+};
+
+export const useAuthContext = () => {
+  const authContext = React.useContext(AuthContext);
+
+  if (authContext) {
+    return authContext;
+  }
+
+  throw new Error(`useAuthContext must be used within a AuthContextProvider`);
+};
