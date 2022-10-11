9dda69b60189c08cb8193859efa29e9b4614fef4
Nathan Panchout <nathanpanchout@live.fr>
2022-10-11 15:41:14 +0200

    🚨(frontend) lint on old files
    pass the linter on several files

--- src/frontend/scripts/src/generateComponent.ts
+++ src/frontend/scripts/src/generateComponent.ts
@@ -1,7 +1,7 @@
-import fs from "fs";
-import path from "path";
+import fs from 'fs';
+import path from 'path';
 
-import { pathToComponents } from "./componentPath";
+import { pathToComponents } from './componentPath';
 import {
   componentTemplate,
   defaultAsTemplate,
@@ -9,70 +9,63 @@ import {
   indexTemplate,
   storiesTemplate,
   testTemplate,
-} from "./generateComponent.templates";
-import appendToFile from "./tools/appendToFile";
-import createDirectory from "./tools/createDirectory";
-import { folderName } from "./tools/nameFormatting";
-import searchInFile from "./tools/searchInFile";
-import writeFile from "./tools/writeFile";
+} from './generateComponent.templates';
+import appendToFile from './tools/appendToFile';
+import createDirectory from './tools/createDirectory';
+import { folderName } from './tools/nameFormatting';
+import searchInFile from './tools/searchInFile';
+import writeFile from './tools/writeFile';
 
 if (!process.argv[2])
   throw new Error(
-    "No component name or path provided. You should provide a component name as an argument."
+    'No component name or path provided. You should provide a component name as an argument.',
   );
 if (process.argv[3])
   throw new Error(
-    "You provided a second argument, but it is not supported. If you want to genereate a component inside a folder, you should use '<Group>/<ComponentName>' as argument."
+    "You provided a second argument, but it is not supported. If you want to genereate a component inside a folder, you should use '<Group>/<ComponentName>' as argument.",
   );
 
-const givenComponentDirs = process.argv[2].split("/").filter((x) => x != "");
+const givenComponentDirs = process.argv[2].split('/').filter((x) => x != '');
 const componentName = givenComponentDirs[givenComponentDirs.length - 1];
 
 const notInCamelCase = givenComponentDirs.filter(
   (folder) =>
-    folder[0] !== folder[0].toUpperCase() ||
-    folder !== folder.replace(/[^a-zA-Z0-9]/g, "")
+    folder[0] !== folder[0].toUpperCase() || folder !== folder.replace(/[^a-zA-Z0-9]/g, ''),
 );
 if (notInCamelCase.length > 0)
   throw new Error(
     `Component name and groups should be in CamelCase. You should use 'FooBar' instead of 'fooBar' or 'foo-bar.
-Invalid folders/components:${notInCamelCase.join(", ")}\n`
+Invalid folders/components:${notInCamelCase.join(', ')}\n`,
   );
 
 const componentDirsNames = givenComponentDirs.map((folder, index) =>
-  index !== givenComponentDirs.length - 1 ? folderName(folder) : folder
+  index !== givenComponentDirs.length - 1 ? folderName(folder) : folder,
 );
 
-const pathToComponentFolderInComponents = path.join(
-  pathToComponents,
-  ...componentDirsNames
-);
+const pathToComponentFolderInComponents = path.join(pathToComponents, ...componentDirsNames);
 
 // CREATE FOLDER
 createDirectory(pathToComponentFolderInComponents);
 
 // CREATE INDEX
-writeFile(
-  path.join(pathToComponentFolderInComponents, "index.ts"),
-  indexTemplate(componentName)
-);
+writeFile(path.join(pathToComponentFolderInComponents, 'index.tsx'), indexTemplate(componentName));
 
 // CREATE COMPONENT
 writeFile(
   path.join(pathToComponentFolderInComponents, `${componentName}.tsx`),
-  componentTemplate(componentName)
+  componentTemplate(componentName),
 );
 
 // CREATE STORIES
 writeFile(
   path.join(pathToComponentFolderInComponents, `${componentName}.stories.tsx`),
-  storiesTemplate(componentName, givenComponentDirs)
+  storiesTemplate(componentName, givenComponentDirs),
 );
 
 // CREATE TEST
 writeFile(
   path.join(pathToComponentFolderInComponents, `${componentName}.test.tsx`),
-  testTemplate(componentName)
+  testTemplate(componentName),
 );
 
 // CREATE INDEX.TS RECURSIVELY
@@ -85,23 +78,21 @@ for (let i = 0; i < componentDirsNames.length - 1; i++) {
 
 // Append 'default as' export to last index file
 appendToFile(
-  path.join(indexesFolder[indexesFolder.length - 1], "index.ts"),
-  defaultAsTemplate(componentName)
+  path.join(indexesFolder[indexesFolder.length - 1], 'index.tsx'),
+  defaultAsTemplate(componentName),
 );
 
 // Append 'export all' export to others
 for (let i = 0; i < indexesFolder.length - 1; i++) {
-  const indexPath = path.join(indexesFolder[i], "index.ts");
+  const indexPath = path.join(indexesFolder[i], 'index.tsx');
   const exportAll = exportAllTemplate(componentDirsNames[i]);
   let findExportAll = -1;
 
   try {
-    const data = fs.readFileSync(indexPath).toString().split("\n");
+    const data = fs.readFileSync(indexPath).toString().split('\n');
     findExportAll = searchInFile(data, exportAll);
   } catch (error: any) {
-    console.log(
-      `Could not search for ${exportAll} in ${indexPath}: ${error.message}`
-    );
+    console.log(`Could not search for ${exportAll} in ${indexPath}: ${error.message}`);
   }
 
   if (findExportAll < 0) {
--- src/frontend/scripts/src/generateIcon.ts
+++ src/frontend/scripts/src/generateIcon.ts
@@ -1,43 +1,38 @@
-import path from "path";
+import path from 'path';
 
-import { pathToComponents } from "./componentPath";
+import { pathToComponents } from './componentPath';
 import {
   exportTemplate,
   iconTemplate,
   indexTemplate,
   lineToFindInStories,
   storyTemplate,
-} from "./generateIcon.templates";
-import appendToFile from "./tools/appendToFile";
-import createDirectory from "./tools/createDirectory";
-import writeFile from "./tools/writeFile";
+} from './generateIcon.templates';
+import appendToFile from './tools/appendToFile';
+import createDirectory from './tools/createDirectory';
+import writeFile from './tools/writeFile';
 
 const iconName = process.argv[2];
 if (!iconName)
-  throw new Error(
-    "No icon name provided. You should provide an icon name as an argument."
-  );
+  throw new Error('No icon name provided. You should provide an icon name as an argument.');
 
-const fullPath = path.join(
-  pathToComponents,
-  `/design-system/svg-icons/${iconName}`
-);
+const fullPath = path.join(pathToComponents, `/design-system/svg-icons/${iconName}`);
 
 createDirectory(fullPath);
 
 // INDEX.TS
-writeFile(path.join(fullPath, "index.ts"), indexTemplate(iconName));
+writeFile(path.join(fullPath, 'index.tsx'), indexTemplate(iconName));
 
 // ICONNAME.TSX
 writeFile(path.join(fullPath, `${iconName}.tsx`), iconTemplate(iconName));
 
 // EXPORT IN ICONS' INDEX.TS
-appendToFile(path.join(fullPath, "../index.ts"), exportTemplate(iconName));
+appendToFile(path.join(fullPath, '../index.tsx'), exportTemplate(iconName));
 
 // ADD ICON IN STORY
 appendToFile(
-  path.join(fullPath, "../SVGIcon.stories.tsx"),
+  path.join(fullPath, '../SVGIcon.stories.tsx'),
   storyTemplate(iconName),
   lineToFindInStories,
-  false
+  false,
 );
