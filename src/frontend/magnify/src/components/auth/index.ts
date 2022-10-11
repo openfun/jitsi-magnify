bcb57bef5739288eba7554ef4b38cd477e51ae58
Nathan Panchout <nathanpanchout@live.fr>
2022-10-11 15:14:11 +0200

    ✨(frontend) export new components
    export new components from magnify Package

--- src/frontend/magnify/src/components/auth/index.ts
+++ src/frontend/magnify/src/components/auth/index.ts
@@ -2,5 +2,5 @@ export { default as LoginForm } from './LoginForm';
 export { default as SignupForm } from './SignupForm';
 export { default as IntroductionLayout } from './IntroductionLayout';
 export { default as AuthForms } from './AuthForms';
-export { default as AuthGard } from './AuthGard';
 export { default as UserMenu } from './UserMenu';
+export * from './RequireUser';
--- src/frontend/magnify/src/index.ts
+++ src/frontend/magnify/src/index.ts
@@ -2,3 +2,7 @@ export * from './components';
 export * from './controller';
 export * from './i18n';
 export * from './themes';
+export * from './middleware';
+export * from './context';
+export * from './utils/routes/api';
+export * from './views';
