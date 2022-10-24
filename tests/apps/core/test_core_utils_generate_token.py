5d5f92c39738d0082a2ae892b120a83568f59f81
jbpenrath <jb.penrath@gmail.com>
2022-10-24 19:00:03 +0200

    ✅(backend) update jwt tokens within tests
    With upgrade of PyJWT 2.6.0 jwt token header has changed, so we have to update
    our tests comparing jwt tokens.

--- tests/apps/core/test_core_utils_generate_token.py
+++ tests/apps/core/test_core_utils_generate_token.py
@@ -3,15 +3,14 @@ Test suite for redirection to jitsi
 """
 import datetime
 import random
-from unittest import mock
 
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.test import TestCase
 from django.test.utils import override_settings
-from django.utils import timezone
 
 import jwt
+import time_machine
 
 from magnify.apps.core.factories import UserFactory
 from magnify.apps.core.utils import generate_token
@@ -31,29 +30,27 @@ from magnify.apps.core.utils import generate_token
 class TokenUtilsTestCase(TestCase):
     """Test suite for utils related to the JWT token."""
 
+    @time_machine.travel(datetime.datetime(2030, 6, 15, tzinfo=datetime.timezone.utc))
     def test_utils_generate_token_anonymous(self):
         """Generate a JWT token for a guest."""
-        now = datetime.datetime(2030, 6, 15, tzinfo=datetime.timezone.utc)
 
         # Test token with a fix admin status
-        with mock.patch.object(timezone, "now", return_value=now):
-            token = generate_token(AnonymousUser(), "my-room", is_admin=True)
+        token = generate_token(AnonymousUser(), "my-room", is_admin=True)
 
         self.assertEqual(
             token,
             (
-                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE5MDc3MTI2MDAsImlhdCI6MTkwNzcxMjA"
-                "wMCwibW9kZXJhdG9yIjp0cnVlLCJhdWQiOiJqaXRzaSIsImlzcyI6ImFwcF9pZCIsInN1YiI6Im1lZXQ"
-                "uaml0c2kiLCJyb29tIjoibXktcm9vbSIsImNvbnRleHQiOnsidXNlciI6eyJhdmF0YXIiOiJhdmF0YXI"
-                "uanBnIiwibmFtZSI6Imd1ZXN0IiwiZW1haWwiOiIifX19.yzGbUJywsVqL0WAbzslIoUovLuAKeLBqXs"
-                "N3h0sPaMo"
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE5MDc3MTI2MDAsImlhdCI6M"
+                "TkwNzcxMjAwMCwibW9kZXJhdG9yIjp0cnVlLCJhdWQiOiJqaXRzaSIsImlzcyI6ImFwcF9"
+                "pZCIsInN1YiI6Im1lZXQuaml0c2kiLCJyb29tIjoibXktcm9vbSIsImNvbnRleHQiOnsid"
+                "XNlciI6eyJhdmF0YXIiOiJhdmF0YXIuanBnIiwibmFtZSI6Imd1ZXN0IiwiZW1haWwiOiI"
+                "ifX19.mCcDVhhWyWfOEGTpiNdemQlPURTCFGh-AWOrLFY2Tds"
             ),
         )
 
         # Test payload with a random admin status
         is_admin = random.choice([True, False])  # nosec
-        with mock.patch.object(timezone, "now", return_value=now):
-            token = generate_token(AnonymousUser(), "my-room", is_admin=is_admin)
+        token = generate_token(AnonymousUser(), "my-room", is_admin=is_admin)
 
         payload = jwt.decode(
             token,
@@ -77,30 +74,28 @@ class TokenUtilsTestCase(TestCase):
             },
         )
 
+    @time_machine.travel(datetime.datetime(2030, 6, 15, tzinfo=datetime.timezone.utc))
     def test_utils_generate_token_authenticated(self):
         """Generate a token for a quidam user."""
         user = UserFactory(username="mickael", email="mickael@example.com")
-        now = datetime.datetime(2030, 6, 15, tzinfo=datetime.timezone.utc)
 
         # Test token with a fix admin status
-        with mock.patch.object(timezone, "now", return_value=now):
-            token = generate_token(user, "my-room", is_admin=True)
+        token = generate_token(user, "my-room", is_admin=True)
 
         self.assertEqual(
             token,
             (
-                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE5MDc3MTI2MDAsImlhdCI6MTkwNzcxMjA"
-                "wMCwibW9kZXJhdG9yIjp0cnVlLCJhdWQiOiJqaXRzaSIsImlzcyI6ImFwcF9pZCIsInN1YiI6Im1lZXQ"
-                "uaml0c2kiLCJyb29tIjoibXktcm9vbSIsImNvbnRleHQiOnsidXNlciI6eyJhdmF0YXIiOiJhdmF0YXI"
-                "uanBnIiwibmFtZSI6Im1pY2thZWwiLCJlbWFpbCI6Im1pY2thZWxAZXhhbXBsZS5jb20ifX19.sU6tEi"
-                "0SKOAheTcS4BCXMhYz1ntiTvKi1uAAiuPNP3k"
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE5MDc3MTI2MDAsImlhdCI6M"
+                "TkwNzcxMjAwMCwibW9kZXJhdG9yIjp0cnVlLCJhdWQiOiJqaXRzaSIsImlzcyI6ImFwcF9"
+                "pZCIsInN1YiI6Im1lZXQuaml0c2kiLCJyb29tIjoibXktcm9vbSIsImNvbnRleHQiOnsid"
+                "XNlciI6eyJhdmF0YXIiOiJhdmF0YXIuanBnIiwibmFtZSI6Im1pY2thZWwiLCJlbWFpbCI"
+                "6Im1pY2thZWxAZXhhbXBsZS5jb20ifX19.0pKgUY7362CaQZYXYgAysot431giCertbe4B"
+                "IOlJ3_8"
             ),
         )
-
         # Test payload with a random admin status
         is_admin = random.choice([True, False])  # nosec
-        with mock.patch.object(timezone, "now", return_value=now):
-            token = generate_token(user, "my-room", is_admin=is_admin)
+        token = generate_token(user, "my-room", is_admin=is_admin)
 
         payload = jwt.decode(
             token,
@@ -128,25 +123,24 @@ class TokenUtilsTestCase(TestCase):
             },
         )
 
+    @time_machine.travel(datetime.datetime(2030, 6, 15, tzinfo=datetime.timezone.utc))
     def test_utils_generate_token_staff(self):
         """Generate a token for a staff user."""
         user = UserFactory(
             username="mickael", email="mickael@example.com", is_staff=True
         )
-        now = datetime.datetime(2030, 6, 15, tzinfo=datetime.timezone.utc)
         is_admin = random.choice([True, False])  # nosec
-
-        with mock.patch.object(timezone, "now", return_value=now):
-            token = generate_token(user, "my-room", is_admin=is_admin)
+        token = generate_token(user, "my-room", is_admin=is_admin)
 
         self.assertEqual(
             token,
             (
-                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE5MDc3MTI2MDAsImlhdCI6MTkwNzcxMjA"
-                "wMCwibW9kZXJhdG9yIjp0cnVlLCJhdWQiOiJqaXRzaSIsImlzcyI6ImFwcF9pZCIsInN1YiI6Im1lZXQ"
-                "uaml0c2kiLCJyb29tIjoibXktcm9vbSIsImNvbnRleHQiOnsidXNlciI6eyJhdmF0YXIiOiJhdmF0YXI"
-                "uanBnIiwibmFtZSI6Im1pY2thZWwiLCJlbWFpbCI6Im1pY2thZWxAZXhhbXBsZS5jb20ifX19.sU6tEi"
-                "0SKOAheTcS4BCXMhYz1ntiTvKi1uAAiuPNP3k"
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE5MDc3MTI2MDAsImlhdCI6M"
+                "TkwNzcxMjAwMCwibW9kZXJhdG9yIjp0cnVlLCJhdWQiOiJqaXRzaSIsImlzcyI6ImFwcF9"
+                "pZCIsInN1YiI6Im1lZXQuaml0c2kiLCJyb29tIjoibXktcm9vbSIsImNvbnRleHQiOnsid"
+                "XNlciI6eyJhdmF0YXIiOiJhdmF0YXIuanBnIiwibmFtZSI6Im1pY2thZWwiLCJlbWFpbCI"
+                "6Im1pY2thZWxAZXhhbXBsZS5jb20ifX19.0pKgUY7362CaQZYXYgAysot431giCertbe4B"
+                "IOlJ3_8"
             ),
         )
 
