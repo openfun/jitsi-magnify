5ab86c713d2a2becf21333e309469b96465effac
karabij <blobfish@helloworld.com>
2022-11-24 13:11:00 +0100

    ✅(frontend) add test for copy link notification
    The appearance of a notification once
    the user clicks on copy link is now tested.

--- src/frontend/magnify/src/components/rooms/RoomRow/RoomRow.test.tsx
+++ src/frontend/magnify/src/components/rooms/RoomRow/RoomRow.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import createRandomRoom from '../../../factories/rooms';
 import { Room } from '../../../types/entities/room';
@@ -12,5 +13,15 @@ describe('RoomRow', () => {
       wrapper: MagnifyTestingProvider,
     });
     screen.getByText(room.name ?? '');
+  });
+  it('should display notification when link was copied to clipboard', async () => {
+    const room: Room = createRandomRoom();
+    const user = userEvent.setup();
+    render(<RoomRow baseJitsiUrl={'meeting.education'} room={room} />, {
+      wrapper: MagnifyTestingProvider,
+    });
+    await user.click(screen.getByRole('button', { name: 'Open Menu' }));
+    await user.click(screen.getByRole('menuitem', { name: 'Clone Copy link' }));
+    await screen.findByText('Room link copied to clipboard!');
   });
 });
