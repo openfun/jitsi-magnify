e55ce88edd6d66de4e01d4a40821859b5d31e5b6
Nathan Panchout <nathanpanchout@live.fr>
2022-12-05 15:23:27 +0100

    🔀(frontend) rebase main
    With the addition of turbo repo, the changes related to the front in
    hand do not merge because of the new folder architecture.
    So we have to do it case by case

--- src/frontend/magnify/packages/components/src/components/rooms/MyRooms/MyRooms.test.tsx
+++ src/frontend/magnify/packages/components/src/components/rooms/MyRooms/MyRooms.test.tsx
@@ -11,4 +11,10 @@ describe('MyRooms', () => {
     // 3) Check the rooms
     expect(screen.getAllByRole('button', { name: 'Join' }).length).toBe(1);
   });
+  it('should display message when rooms list is emty', async () => {
+    render(<MyRooms baseJitsiUrl="" rooms={[]} />);
+    await screen.findByText(
+      'No room was created yet. Click on the button " + Room" to create one.',
+    );
+  });
 });
