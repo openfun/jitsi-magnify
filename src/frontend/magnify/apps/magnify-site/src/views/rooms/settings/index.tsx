5e91a7febe5c00631abe225cc68bdf31880c67d7
Nathan Panchout <nathanpanchout@live.fr>
2022-12-19 17:29:27 +0100

    ✨(frontend) integration in the demo
    We integrate the feature in the demo by adding all the
    necessary methods and components

--- src/frontend/magnify/apps/magnify-site/src/views/rooms/settings/index.tsx
+++ src/frontend/magnify/apps/magnify-site/src/views/rooms/settings/index.tsx
@@ -1,11 +1,14 @@
 import {
   MagnifyQueryKeys,
+  RoomAccessRole,
   RoomConfig,
   RoomsRepository,
+  RoomUsersConfig,
+  User,
   useTranslations,
 } from '@openfun/magnify-components';
-import { useQuery } from '@tanstack/react-query';
-import { Spinner } from 'grommet';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { Box, Spinner } from 'grommet';
 import * as React from 'react';
 import { defineMessages } from 'react-intl';
 import { useParams } from 'react-router-dom';
@@ -22,15 +25,59 @@ export const roomSettingsMessages = defineMessages({
 export function RoomSettingsView() {
   const intl = useTranslations();
   const { id } = useParams();
-
+  const queryClient = useQueryClient();
   const { data: room, isLoading } = useQuery([MagnifyQueryKeys.ROOM, id], () => {
     return RoomsRepository.get(id);
   });
 
+  const invalidateAllQueries = (): void => {
+    queryClient.invalidateQueries([MagnifyQueryKeys.ROOM, id]);
+    queryClient.invalidateQueries([MagnifyQueryKeys.ROOM]);
+  };
+
+  const addUser = (user: User): void => {
+    if (!room) {
+      return;
+    }
+    RoomsRepository.addUser(room.id, RoomAccessRole.MEMBER, user.id).then(() => {
+      invalidateAllQueries();
+    });
+  };
+
+  const updateUser = (role: RoomAccessRole, userId: string, accessId: string): void => {
+    if (!room) {
+      return;
+    }
+    RoomsRepository.updateUser(room.id, role, userId, accessId).then(() => {
+      invalidateAllQueries();
+    });
+  };
+
+  const deleteUser = (accessId: string): void => {
+    if (!room) {
+      return;
+    }
+    RoomsRepository.deleteUser(accessId).then(() => {
+      invalidateAllQueries();
+    });
+  };
+
   return (
     <DefaultPage title={intl.formatMessage(roomSettingsMessages.roomSettingsTitle)}>
       {isLoading && <Spinner />}
-      {!isLoading && room && <RoomConfig room={room} />}
+      {!isLoading && room && (
+        <>
+          <RoomConfig room={room} />
+          <Box margin={'15px 0'}>
+            <RoomUsersConfig
+              addUser={addUser}
+              onDeleteUser={deleteUser}
+              onUpdateUser={updateUser}
+              room={room}
+            />
+          </Box>
+        </>
+      )}
     </DefaultPage>
   );
 }
