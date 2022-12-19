c21cb12cda53383ace8c7d35b29fe81ed5f6f5b3
Nathan Panchout <nathanpanchout@live.fr>
2022-12-19 17:08:59 +0100

    ✨(frontend) add simple list component
    For this feature we need to display users in a list.
    So we add a generic MagnifyList component usable with any row type

---
+++ src/frontend/magnify/packages/components/src/components/design-system/List/MagnifyList.tsx
@@ -0,0 +1,45 @@
+import { Box } from 'grommet';
+import * as React from 'react';
+import { useState } from 'react';
+
+export interface Row {
+  id: string;
+}
+
+export interface RowPropsExtended<T> {
+  item: T;
+  selected: boolean;
+  onToggle: () => void;
+}
+
+export interface MagnifyListProps<TRowProps extends Row> {
+  rows: TRowProps[];
+  Row: React.FC<RowPropsExtended<TRowProps>>;
+}
+
+export default function MagnifyList<TRowProps extends Row>({
+  rows,
+  Row,
+  ...props
+}: MagnifyListProps<TRowProps>) {
+  const [selected, setSelected] = useState<Record<string, boolean>>(
+    Object.fromEntries(rows.map((row) => [row.id, false])),
+  );
+
+  const handleToggle = (groupId: string) => {
+    setSelected((pSelected) => ({ ...pSelected, [groupId]: !pSelected[groupId] }));
+  };
+
+  return (
+    <Box gap={'10px'}>
+      {rows.map((row) => (
+        <Row
+          key={row.id}
+          item={row}
+          onToggle={() => handleToggle(row.id)}
+          selected={selected[row.id]}
+        />
+      ))}
+    </Box>
+  );
+}
