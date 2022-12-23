c6b024642e757018a12e3b7d8d2ccf35e52a2814
Samuel Paccoud - France Université Numérique <samuel.paccoud@fun-mooc.fr>
2022-12-23 12:33:28 +0100

    ✨(admin) add room model to Django admin
    We want to be able to administer rooms from the admin.

--- src/magnify/apps/core/admin.py
+++ src/magnify/apps/core/admin.py
@@ -1,9 +1,23 @@
 """Admin classes and registrations for Magnify's core app."""
 from django.contrib import admin
 
-from .models import User
+from .models import ResourceAccess, Room, User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """User admin interface declaration."""
+
+
+class ResourceAccessInline(admin.TabularInline):
+    """Admin class for the room user access model"""
+
+    model = ResourceAccess
+    extra = 0
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    """Room admin interface declaration."""
+
+    inlines = (ResourceAccessInline,)
