b6cbaea0253e5386a0f67d063f5d181061bfb8b8
nathanpanchout <nathanpanchout@live.fr>
2023-01-23 11:18:49 +0100

    🐛(frontend) fix dropdown width
    The width of a dropdown is set today in relation to the width of the parent,
    but on very small parents this is not enough.

--- src/frontend/magnify/packages/components/src/components/design-system/Layout/Header/Avatar/ResponsiveLayoutHeaderAvatar.tsx
+++ src/frontend/magnify/packages/components/src/components/design-system/Layout/Header/Avatar/ResponsiveLayoutHeaderAvatar.tsx
@@ -32,7 +32,7 @@ export const ResponsiveLayoutHeaderAvatar: FunctionComponent<ResponsiveLayoutHea
   return (
     <Box ref={avatarRef} align={'center'} direction={'row'} justify={'center'}>
       <Menu
-        dropProps={{ align: { top: 'bottom', right: 'right' } }}
+        dropProps={{ stretch: false, align: { top: 'bottom', right: 'right' } }}
         items={[
           {
             icon: <User />,
