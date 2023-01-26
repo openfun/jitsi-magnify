1f26b29c34d26118819584be7359ed5c9586581f
jbpenrath <jb.penrath@gmail.com>
2023-01-26 17:04:05 +0100

    🔧(magnify-site) disable treeshaking for production build
    Currently the production build does not work as rollup treeshake remove from
    bundle the ThemeContext module provided by grommet. For now, the only
    workaround, we found is to disable treeshake.

--- src/frontend/magnify/apps/magnify-site/vite.config.ts
+++ src/frontend/magnify/apps/magnify-site/vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // grommet declares in its package.json a sideEffects attribute which targets
+      // ThemeContext module. For still unknown reason, this module is removed during
+      // the rollup tree shaking. As a workaround, we disable the tree shaking.
+      // The drawback is that the bundle size is bigger (about 9%).
+      treeshake: false,
+    },
+  },
   server: {
     host: true,
     port: 3200,
