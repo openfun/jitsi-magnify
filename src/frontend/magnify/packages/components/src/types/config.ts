e3eeace0675e052a18105f80920d2855577ce46d
nathanpanchout <nathanpanchout@live.fr>
2023-04-14 14:33:26 +0200

    ✨(frontend) hide element when MAGNIFY_SHOW_REGISTER_LINK is false
    We need to hide the "register" link and the list of rooms when the
    MAGNIFY_SHOW_REGISTER_LINK config is false

--- src/frontend/magnify/packages/components/src/types/config.ts
+++ src/frontend/magnify/packages/components/src/types/config.ts
@@ -5,6 +5,7 @@ export interface MagnifyConfiguration {
   KEYCLOAK_CLIENT_ID: string;
   KEYCLOAK_URL: string;
   KEYCLOAK_EXPIRATION_SECONDS: number;
+  MAGNIFY_SHOW_REGISTER_LINK: boolean;
   KEYCLOAK_REALM: string;
   JITSI_DOMAIN: string;
   LANGUAGE_CODE: string;
@@ -15,6 +16,7 @@ export const TESTING_CONF: MagnifyConfiguration = {
   JITSI_DOMAIN: 'meeting.education',
   KEYCLOAK_CLIENT_ID: 'magnify-front',
   LANGUAGE_CODE: MagnifyLocales.EN,
+  MAGNIFY_SHOW_REGISTER_LINK: true,
   KEYCLOAK_EXPIRATION_SECONDS: 1800,
   KEYCLOAK_REALM: 'magnify',
   KEYCLOAK_URL: 'http://localhost:8080',
