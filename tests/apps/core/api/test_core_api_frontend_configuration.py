9837790be100ab13705a1ed1ee6ddd13a43bef09
Samuel Paccoud - France Université Numérique <samuel.paccoud@fun-mooc.fr>
2023-04-14 12:35:49 +0200

    ✨(frontend) allow hiding the register link
    Some identity federations to which Magnify will be connected do not
    allow registration. We need to hide the "register" link in this case.

--- tests/apps/core/api/test_core_api_frontend_configuration.py
+++ tests/apps/core/api/test_core_api_frontend_configuration.py
@@ -9,19 +9,7 @@ from rest_framework.test import APITestCase
 class FrontendConfigurationTestCase(APITestCase):
     """Test requests on magnify's core app frontend configuration endpoint."""
 
-    @override_settings(
-        JITSI_CONFIGURATION={
-            "jitsi_domain": "meeting.education",
-        },
-        FRONTEND_CONFIGURATION={
-            "API_URL": "http://localhost:8070/api",
-            "KEYCLOAK_URL": "http://localhost:8080",
-            "KEYCLOAK_REALM": "magnify",
-            "KEYCLOAK_CLIENT_ID": "magnify-front",
-            "KEYCLOAK_EXPIRATION_SECONDS": 1800,
-        },
-    )
-    def test_api_get_frontend_configuration(self):
+    def test_api_get_frontend_configuration_default(self):
         """Any user should be able to get frontend configuration."""
 
         response = self.client.get("/api/config.json")
@@ -36,5 +24,37 @@ class FrontendConfigurationTestCase(APITestCase):
                 "KEYCLOAK_CLIENT_ID": "magnify-front",
                 "KEYCLOAK_EXPIRATION_SECONDS": 1800,
                 "LANGUAGE_CODE": "en",
+                "SHOW_REGISTER_LINK": True,
+            },
+        )
+
+    @override_settings(
+        JITSI_CONFIGURATION={
+            "jitsi_domain": "example.com",
+        },
+        FRONTEND_CONFIGURATION={
+            "API_URL": "http://api.example.com/api",
+            "KEYCLOAK_URL": "http://kc.example.com",
+            "KEYCLOAK_REALM": "example",
+            "KEYCLOAK_CLIENT_ID": "example-front",
+            "KEYCLOAK_EXPIRATION_SECONDS": 18000,
+            "LANGUAGE_CODE": "fr",
+        },
+    )
+    def test_api_get_frontend_configuration_from_settings(self):
+        """Any user should be able to get frontend configuration."""
+
+        response = self.client.get("/api/config.json")
+
+        self.assertEqual(
+            response.json(),
+            {
+                "API_URL": "http://api.example.com/api",
+                "JITSI_DOMAIN": "example.com",
+                "KEYCLOAK_URL": "http://kc.example.com",
+                "KEYCLOAK_REALM": "example",
+                "KEYCLOAK_CLIENT_ID": "example-front",
+                "KEYCLOAK_EXPIRATION_SECONDS": 18000,
+                "LANGUAGE_CODE": "fr",
             },
         )
