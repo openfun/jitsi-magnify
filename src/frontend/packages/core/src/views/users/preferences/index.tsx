ca23da7d6f031a653bd6f77e65d34bb9102c2d09
nathanpanchout <nathanpanchout@live.fr>
2023-05-26 11:05:25 +0200

    ✨(frontend) create BackButton
    In several pages, we have a back button. However the behavior was duplicated.
    So we created and used a <BackButton/> component

--- src/frontend/packages/core/src/views/users/preferences/index.tsx
+++ src/frontend/packages/core/src/views/users/preferences/index.tsx
@@ -1,10 +1,9 @@
-import { Button, Heading } from 'grommet';
+import { Box, Heading } from 'grommet';
 import * as React from 'react';
 import { defineMessages } from 'react-intl';
-import { useNavigate } from 'react-router-dom';
 import { CustomCard, UserProfilePreferences } from '../../../components';
+import { BackButton } from '../../../components/design-system/Button/Back/BackButton';
 import { useTranslations } from '../../../i18n';
-import { commonMessages } from '../../../i18n/commonMessages';
 
 const messages = defineMessages({
   title: {
@@ -14,25 +13,19 @@ const messages = defineMessages({
   },
 });
 export const UserPreference = () => {
-  const navigate = useNavigate();
   const intl = useTranslations();
 
   return (
-    <>
-      <Button
-        color={'brand'}
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        {intl.formatMessage(commonMessages.back)}
-      </Button>
+    <Box gap={'small'}>
+      <div>
+        <BackButton />
+      </div>
       <CustomCard>
         <Heading color={'brand'} level={4} margin={'none'}>
           {intl.formatMessage(messages.title)}
         </Heading>
         <UserProfilePreferences />
       </CustomCard>
-    </>
+    </Box>
   );
 };
