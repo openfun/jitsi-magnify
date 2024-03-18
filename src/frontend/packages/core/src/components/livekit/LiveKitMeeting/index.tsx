23b56105e7cbe4daefaf2e5f2b2a4e56b6b2cab8
Raphaël FAURE <raphael.faure2@student-cs.fr>
2024-03-18 15:27:01 +0100

    (feat) Implement join and mute notification

--- src/frontend/packages/core/src/components/livekit/LiveKitMeeting/index.tsx
+++ src/frontend/packages/core/src/components/livekit/LiveKitMeeting/index.tsx
@@ -4,8 +4,9 @@ import { MagnifyControlBar } from '../controls/bar';
 import '@livekit/components-styles';
 import { useState } from 'react';
 import { ConferenceLayout } from '../conference/conference';
-import {ParticipantsLayout, ParticipantLayoutContext } from '../controls/participants';
+import { ParticipantsLayout, ParticipantLayoutContext } from '../controls/participants';
 import { RoomService, RoomServices } from '../../../services/livekit/room.services';
+import { EventHandlerProvider } from '../../../services/livekit/events';
 
 
 const OpenerIcon = () =>
@@ -30,26 +31,27 @@ export const LiveKitMeeting = ({
         showChat: false,
         unreadMessages: 0,
     });
-
     return (
+        <EventHandlerProvider>
         <RoomServiceContext token={props.token}>
             <ParticipantLayoutContext visible={true}>
                 <LayoutContextProvider onWidgetChange={setWidgetState}>
                     <div style={{ maxHeight: "100vh", height: "100%", display: "grid", gridTemplateRows: "10fr 1fr", gridTemplateColumns: "0fr 6fr 0fr" }}>
-                        <ParticipantsLayout style={{ gridRow: "1/2", gridColumn: "1/2", overflow:"hidden" }} />
-                        <div style={{ position: "relative", gridRow: "1/2", gridColumn: "2/3", overflow:"hidden" }}>
+                        <ParticipantsLayout style={{ gridRow: "1/2", gridColumn: "1/2", overflow: "hidden" }} />
+                        <div style={{ position: "relative", gridRow: "1/2", gridColumn: "2/3", overflow: "hidden" }}>
                             <ConferenceLayout />
                         </div>
-                        <Chat  className={"test"} style={{ display: widgetState.showChat ? 'grid' : 'none', gridRow: "1/2", gridColumn: "3/4", width: "20vw", overflow:"hidden" }} />
+                        <Chat className={"test"} style={{ display: widgetState.showChat ? 'grid' : 'none', gridRow: "1/2", gridColumn: "3/4", width: "20vw", overflow: "hidden" }} />
 
                         <div style={{ gridRow: "2/3", gridTemplateColumns: "1fr 10fr", gridColumn: "1/4" }}>
-                            <MagnifyControlBar/>
+                            <MagnifyControlBar />
                         </div>
                     </div>
                     <RoomAudioRenderer></RoomAudioRenderer>
                 </LayoutContextProvider>
             </ParticipantLayoutContext>
         </RoomServiceContext>
+        </EventHandlerProvider>
 
     )
 }
