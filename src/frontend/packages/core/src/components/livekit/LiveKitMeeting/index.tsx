2830a8ef16c1213744f393ee5d125d1574a16b16
Houdin Thomas <houdinthomas@Houdins-Laptop.local>
2024-03-26 16:09:12 +0100

    fix code not supposed to exist

--- src/frontend/packages/core/src/components/livekit/LiveKitMeeting/index.tsx
+++ src/frontend/packages/core/src/components/livekit/LiveKitMeeting/index.tsx
@@ -133,3 +133,30 @@ const RoomServiceContext = (props: any) => {
             {props.children}
         </RoomServices.Provider>
     )
+}
+
+const Overlay = () => {
+    const layoutContext = useParticipantLayoutContext()
+    const chatContext = useLayoutContext()
+    return (
+        <>
+            <Box style={{ backgroundColor: "black", position: "fixed", width: "100%", height: "100%", zIndex: 2, display: layoutContext.visible ? "grid" : "none", boxShadow: "1px 5px 5px black" }} elevation='100px'>
+                <ParticipantsLayout />
+            </Box>
+            <Box style={{ backgroundColor: "black", position: "fixed", right: "0", width: "100%", height: "100%", zIndex: 2, display: chatContext.widget.state?.showChat ? "grid" : "none" }}>
+                <Chat style={{ width: "100%", height: "100%", float: "right", backgroundColor: "transparent" }} />
+            </Box>
+        </>
+    )
+}
+
+const MobileHeader = ({...props} : HTMLAttributes<HTMLDivElement>) => {
+    const localP = useLocalParticipant()
+    return (
+        <div {...props} >
+            <ParticipantLayoutToggle style={{backgroundColor:'transparent'}}/>
+            <h4>{localP.localParticipant.name}</h4>
+            <ChatToggle  props={{style:{backgroundColor:'transparent'}}}/>
+        </div>
+    )
+}
